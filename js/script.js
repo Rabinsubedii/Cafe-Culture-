2aedba72d9f83914fd15e473011141451e31c481
codirabin codirabin <rabbinsubedi@gmail.com>
2022-06-03 12:20:05 +0545

    done

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 // // customer feedback testom
-$(document).ready(function() {
-    $(".testimonial .indicators li").click(function() {
+$(document).ready(function () {
+    $(".testimonial .indicators li").click(function () {
         var i = $(this).index();
         var targetElement = $(".testimonial .tabs li");
         targetElement.eq(i).addClass('active');
         targetElement.not(targetElement[i]).removeClass('active');
     });
-    $(".testimonial .tabs li").click(function() {
+    $(".testimonial .tabs li").click(function () {
         var targetElement = $(".testimonial .tabs li");
         targetElement.addClass('active');
         targetElement.not($(this)).removeClass('active');
     });
 });
-$(document).ready(function() {
-    $(".slider .swiper-pagination span").each(function(i) {
+$(document).ready(function () {
+    $(".slider .swiper-pagination span").each(function (i) {
         $(this).text(i + 1).prepend("0");
     });
 });
@@ -21,7 +21,7 @@ $(document).ready(function() {
 
 
 // Select all tabs for menu
-$('.nav-tabs a').click(function() {
+$('.nav-tabs a').click(function () {
     $(this).tab('show');
 })
 
@@ -48,3 +48,48 @@ function add_more_field() {
     var form = document.getElementById('ordeer-form')
     form.innerHTML += html
 }
+
+$("[id^=carousel-thumbs]").carousel({
+    interval: false
+});
+
+/** Pause/Play Button **/
+$(".carousel-pause").click(function () {
+    var id = $(this).attr("href");
+    if ($(this).hasClass("pause")) {
+        $(this).removeClass("pause").toggleClass("play");
+        $(this).children(".sr-only").text("Play");
+        $(id).carousel("pause");
+    } else {
+        $(this).removeClass("play").toggleClass("pause");
+        $(this).children(".sr-only").text("Pause");
+        $(id).carousel("cycle");
+    }
+    $(id).carousel;
+});
+
+/** Fullscreen Buttun **/
+$(".carousel-fullscreen").click(function () {
+    var id = $(this).attr("href");
+    $(id).find(".active").ekkoLightbox({
+        type: "image"
+    });
+});
+
+if ($("[id^=carousel-thumbs] .carousel-item").length < 2) {
+    $("#carousel-thumbs [class^=carousel-control-]").remove();
+    $("#carousel-thumbs").css("padding", "0 5px");
+}
+
+$("#carousel").on("slide.bs.carousel", function (e) {
+    var id = parseInt($(e.relatedTarget).attr("data-slide-number"));
+    var thumbNum = parseInt(
+        $("[id=carousel-selector-" + id + "]")
+            .parent()
+            .parent()
+            .attr("data-slide-number")
+    );
+    $("[id^=carousel-selector-]").removeClass("selected");
+    $("[id=carousel-selector-" + id + "]").addClass("selected");
+    $("#carousel-thumbs").carousel(thumbNum);
+});
